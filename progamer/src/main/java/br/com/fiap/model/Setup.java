784b675c3477612226d4e8ddef257891e833af3b
Logon Aluno <logonrmlocal@fiap.com.br>
2022-03-31 11:58:08 -0300

    Adicionando primefaces, bean e model

---
+++ progamer/src/main/java/br/com/fiap/model/Setup.java
@@ -0,0 +1,39 @@
+package br.com.fiap.model;
+
+import java.math.BigDecimal;
+
+public class Setup {
+	private String name = "Meu Setup";
+	private String description = "Descrição";
+	private BigDecimal price = new BigDecimal(2000);
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Setup [name=" + name + ", description=" + description + ", price=" + price + "]";
+	}
+
+}
